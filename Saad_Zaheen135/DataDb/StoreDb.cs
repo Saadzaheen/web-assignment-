ca58f2da394c0dff4287f4a39005603b3acf1731
infou <infou@DESKTOP-ACHRQEC>
2022-01-11 21:43:06 +0500

    Add project files.

---
+++ Saad_Zaheen135/DataDb/StoreDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saad_Zaheen135.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Saad_Zaheen135.DataDb
+{
+    public class StoreDb : DbContext
+    {
+        public StoreDb(DbContextOptions<StoreDb> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderModel> orders { get; set; }
+    }
+}
---
+++ Saad_Zaheen135/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Saad_Zaheen135.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
